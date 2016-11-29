0ad703030e5673ea49f09578784fe0de93fff046
akash1196 <akashp1196@gmail.com>
2016-11-29 00:21:02 -0700

    created temporary object list in keyinput class.

--- src/com/game/main/KeyInput.java
+++ src/com/game/main/KeyInput.java
@@ -2,6 +2,7 @@ package com.game.main;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.LinkedList;
 
 /**
  * Deals with key input for cannon rotation
@@ -9,18 +10,20 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 
     private Handler handler;
+    private LinkedList<GameObject> tempObjectList; // temp list...remove cannon ball when spacebar released
     private double tempTheta = Math.PI/2;
 
     public KeyInput(Handler handler) {
         this.handler = handler;
+        createTempObjectList();
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
 
-        for(int i = 0; i < handler.object.size(); i++){
-            GameObject temp = handler.object.get(i);
+        for(int i = 0; i < tempObjectList.size(); i++){
+            GameObject temp = tempObjectList.get(i);
 
             if(temp.getId() == ID.Cannon){
                 //key events for the cannon object
@@ -33,6 +36,14 @@ public class KeyInput extends KeyAdapter{
                     temp.setVelTheta(Math.toRadians(-2));
                     tempTheta += Math.toRadians(-2);
                     tempTheta = Game.clamp(tempTheta, -1.5, 1.5);
+                }
+            }
+
+            if(temp.getId() == ID.CannonBall) {
+                if(key == KeyEvent.VK_SPACE) {
+                    System.out.println(tempTheta);
+                    temp.setVelX((int)(5 * Math.cos(tempTheta))); // x-component = vel * cos(angle)
+                    temp.setVelY((int)(5 * Math.sin(tempTheta))); // y-component = vel * sin(angle)
                 }
             }
         }
@@ -54,9 +65,23 @@ public class KeyInput extends KeyAdapter{
                     temp.setVelTheta(0);
                 }
             }
+
+            if(temp.getId() == ID.CannonBall) {
+                if(key == KeyEvent.VK_SPACE) {
+                    tempObjectList.remove(temp);
+                }
+            }
         }
     }
 
+    private void createTempObjectList(){
+        tempObjectList = new LinkedList<>();
+        for(int i = 0; i < handler.object.size(); i++){
+            tempObjectList.add(handler.object.get(i));
+        }
+    }
+
+    /**
     @Override
     public void keyTyped(KeyEvent e) {
         char keyChar = e.getKeyChar();
@@ -72,5 +97,5 @@ public class KeyInput extends KeyAdapter{
                 }
             }
         }
-    }
+    }*/
 }
